51652eedfdefb5c83dbb3966ed4f2bc765f13ca9
yura702007 <yura702007@gmail.com>
2024-04-21 05:27:48 +0300

    add paths for password reset to account.urls.urlpatterns

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
