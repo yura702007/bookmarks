43a801d56e6a5f021c9cfa20bbbddb055b61a52a
yura702007 <82802568+yura702007@users.noreply.github.com>
2024-04-23 12:32:44 +0300

    add a method to check the fields “password” and “password2” for equality

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -10,6 +10,11 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'email']
 
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
 
 
 class LoginForm(forms.Form):
