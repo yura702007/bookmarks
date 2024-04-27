9efb57558c12d17df41f05f7af4da2bd8d31723f
yura702007 <yura702007@gmail.com>
2024-04-27 15:18:48 +0300

    add method 'save()' in model 'Images'

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -19,5 +20,10 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 # Create your models here.
