ca2fd25b5e8ff862f68e007974d0b01a1566a6dd
yura702007 <yura702007@gmail.com>
2024-04-27 15:32:55 +0300

    add a many-to-many relationship

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -11,6 +11,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     class Meta:
         indexes = [
