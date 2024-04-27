f16dbe6aede32ab0ee9bfebf8f1e16471575224b
yura702007 <yura702007@gmail.com>
2024-04-27 18:50:29 +0300

    add method save() image in ImageCreateForm

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.Form):
@@ -17,3 +20,16 @@ class ImageCreateForm(forms.Form):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # скачать изображение с данного URL-адреса
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
