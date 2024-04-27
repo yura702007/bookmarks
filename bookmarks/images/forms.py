36a915e15c0c5d6123e0227bf384cdfee7705a28
yura702007 <yura702007@gmail.com>
2024-04-27 18:22:24 +0300

    add check url extension

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,3 +9,11 @@ class ImageCreateForm(forms.Form):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
